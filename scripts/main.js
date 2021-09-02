e0b346fad147527a732e17bb8db4d152b23f41d8
MikkelHebel <88710599+MikkelHebel@users.noreply.github.com>
2021-09-02 09:47:49 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// Roads and coords objects are expected to be loaded
+const ctx = document.querySelector("#town").getContext("2d");
+
+drawLocations();
+drawRoads();
---
+++ scripts/town.js
@@ -0,0 +1,28 @@
+// Build connection object, each key (location) should contain an array of all connected locations
+function build_connections() {
+const connections = build_connections();
+console.log(connections.A);
+// Output: Array(3) [ "B", "C", "P" ]
+
+let packages = [{ current: "A", destination: "B" }, { current: "A",
+destination: "C" }, { current: "A", destination: "G" }];
+
+}
+
+
+// State object template for the town
+const state = {
+    init(robot_loc, packages) {
+        this.robot_loc = robot_loc;
+        this.packages = packages;
+
+    let test = Object.create(state);test.init("A", [{ current: "A", destination: "B" }, { current: "A",destination: "C" }]);
+    let travel = ["B", "A", "C", "P"];
+    for (let to of travel) {    
+      test = test.move(to);
+      console.log(test);
+      }
+    },
+
+    move(to) {}
+};
